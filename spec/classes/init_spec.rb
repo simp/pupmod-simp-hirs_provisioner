7e187d95eb216ac799aa3cbd6c52e1d6c879223d
Michael Morrone <michael.morrone@onyxpoint.com>
2018-12-19 10:45:06 -0500

    Added spec tests for init and install

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -7,10 +7,6 @@ describe 'hirs_provisioner' do
     it { is_expected.to contain_class('hirs_provisioner') }
     it { is_expected.to contain_class('hirs_provisioner::install').that_comes_before('Class[hirs_provisioner::config]') }
     it { is_expected.to contain_class('hirs_provisioner::config') }
-    it { is_expected.to contain_class('hirs_provisioner::service').that_subscribes_to('Class[hirs_provisioner::config]') }
-
-    it { is_expected.to contain_service('hirs_provisioner') }
-    it { is_expected.to contain_package('hirs_provisioner').with_ensure('present') }
   end
 
   context 'supported operating systems' do
@@ -20,60 +16,43 @@ describe 'hirs_provisioner' do
           os_facts
         end
 
-        context "hirs_provisioner class without any parameters" do
-          let(:params) {{ }}
+        context "with default parameters" do
+          let(:facts) do 
+            os_facts.merge({
+              tpm12_enabled: 'false',
+              tpm2_enabled:  'false'
+            })
+          end
+          it { is_expected.to compile.with_all_deps }
+          it { is_expected.to create_class('hirs_provisioner') }
+          it { is_expected.to contain_class('hirs_provisioner') }
+          it { is_expected.not_to contain_class('hirs_provisioner::install') }
+          it { is_expected.not_to contain_class('hirs_provisioner::config') }
+        end
+
+        context "with default parameters and TPM 1.2 detected" do
+          let(:facts) do 
+            os_facts.merge({
+              tpm12_enabled: 'true',
+              tpm2_enabled:  'false',
+              ima_enabled:   'false'
+            })
+          end
           it_behaves_like "a structured module"
-          it { is_expected.to contain_class('hirs_provisioner').with_trusted_nets(['127.0.0.1/32']) }
         end
 
-        context "hirs_provisioner class with firewall enabled" do
-          let(:params) {{
-            :enable_firewall => true
-          }}
-
-          ###it_behaves_like "a structured module"
-          it { is_expected.to contain_class('hirs_provisioner::config::firewall') }
-
-          it { is_expected.to contain_class('hirs_provisioner::config::firewall').that_comes_before('Class[hirs_provisioner::service]') }
-          it { is_expected.to create_iptables__listen__tcp_stateful('allow_hirs_provisioner_tcp_connections').with_dports(9999)
-          }
+        context "with default parameters and TPM 2 detected" do
+          let(:facts) do 
+            os_facts.merge({
+              tpm12_enabled: 'false',
+              tpm2_enabled:  'true',
+              ima_enabled:   'false'
+            })
+          end
+          it_behaves_like "a structured module"
         end
 
-        context "hirs_provisioner class with auditing enabled" do
-          let(:params) {{
-            :enable_auditing => true
-          }}
-
-          ###it_behaves_like "a structured module"
-          it { is_expected.to contain_class('hirs_provisioner::config::auditing') }
-          it { is_expected.to contain_class('hirs_provisioner::config::auditing').that_comes_before('Class[hirs_provisioner::service]') }
-          it { is_expected.to create_notify('FIXME: auditing') }
-        end
-
-        context "hirs_provisioner class with logging enabled" do
-          let(:params) {{
-            :enable_logging => true
-          }}
-
-          ###it_behaves_like "a structured module"
-          it { is_expected.to contain_class('hirs_provisioner::config::logging') }
-          it { is_expected.to contain_class('hirs_provisioner::config::logging').that_comes_before('Class[hirs_provisioner::service]') }
-          it { is_expected.to create_notify('FIXME: logging') }
-        end
       end
-    end
-  end
-
-  context 'unsupported operating system' do
-    describe 'hirs_provisioner class without any parameters on Solaris/Nexenta' do
-      let(:facts) {{
-        :os => {
-          :family => 'Solaris',
-          :name   => 'Nexenta'
-        }
-      }}
-
-      it { expect { is_expected.to contain_package('hirs_provisioner').to raise_error(/OS 'Nexenta' is not supported/) } }
     end
   end
 end
---
+++ spec/classes/install_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'hirs_provisioner::install' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, os_facts|
+      context "on #{os}" do
+        let(:facts) do
+          os_facts
+        end
+
+        context "with default parameters and TPM 1.2 detected" do
+          let(:facts) do
+            os_facts.merge({
+              tpm12_enabled: 'true',
+              tpm2_enabled:  'false',
+              ima_enabled:   'false'
+            })
+          end 
+          let(:pre_condition) { 'include hirs_provisioner' }
+          it { is_expected.to contain_package('HIRS_Provisioner_TPM_1_2').with_ensure('installed') }
+          it { is_expected.to create_file('/var/log/hirs/provisioner').with_ensure('directory') }
+          it { is_expected.not_to create_file('/usr/sbin/hirs-provisioner') }
+        end
+
+        context "with default parameters and TPM 2 detected" do
+          let(:facts) do
+            os_facts.merge({
+              tpm12_enabled: 'false',
+              tpm2_enabled:  'true',
+              ima_enabled:   'false'
+            })
+          end 
+          let(:pre_condition) { 'include hirs_provisioner' }
+          it { is_expected.to contain_package('HIRS_Provisioner_TPM_2_0').with_ensure('installed') }
+          it { is_expected.to create_file('/var/log/hirs/provisioner').with_ensure('directory') }
+          it { is_expected.to create_file('/usr/sbin/hirs-provisioner').with_ensure('link') }
+        end
+
+      end
+    end
+  end
+end
